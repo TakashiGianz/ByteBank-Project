84921cb1022fb05896a7d96d20014543b5a63c8c
Fernando-Escritorio <fernando.takashi12@gmail.com>
2021-07-22 16:39:36 -0300

    Modularizando o código

--- index.js
+++ index.js
@@ -1,28 +1,7 @@
-class Cliente {
-    nome;
-    cpf;
-}
+import { Cliente } from "./Cliente.js";
+import { ContaCorrente } from "./ContaCorrente.js";
 
-class ContaCorrente{
-    agencia;
-    #saldo = 0;
-
-    sacar(valor){
-        if(this.#saldo >= valor){
-            this.#saldo -= valor
-        }
-    }
-
-    depositar(valor){
-        if(valor > 0){
-        this.#saldo += valor
-        console.log(`Seu saldo é de: R$${contaCorrenteFernando.#saldo}`)
-        }
-    }
-
-}
-
-
+// Clientes
 const cliente1 = new Cliente();
 cliente1.nome = "Fernando";
 cliente1.cpf = 47000398841;
@@ -31,13 +10,23 @@ const cliente2 = new Cliente();
 cliente2.nome = "Ellen";
 cliente2.cpf = 48182838860;
 
-const contaCorrenteFernando = new ContaCorrente();
-contaCorrenteFernando.agencia = 1001;
+// Contas Correntes
+const ContaCorrente1 = new ContaCorrente();
+ContaCorrente1.agencia = 1001;
+ContaCorrente1.cliente = cliente1;
 
+const ContaCorrente2 = new ContaCorrente();
+ContaCorrente2.agencia = 2001;
+ContaCorrente2.cliente = cliente2;
 
-contaCorrenteFernando.depositar(2000);
-contaCorrenteFernando.sacar(1500);
+// Ações
+const valorDepositado = ContaCorrente1.depositar(2000); 
+const valorSacado = ContaCorrente1.sacar(1500);
+const valorTransferido = ContaCorrente1.transferir(200, ContaCorrente2);
 
-
-console.log(`Olá ${cliente1.nome}`)
-// console.log(`Seu saldo é de: R$${contaCorrenteFernando.#saldo}`)
+// Visualizações
+console.log(`${cliente1.nome} depositou: R$${valorDepositado}`)
+console.log(`${cliente1.nome} sacou: R$${valorSacado}`)
+console.log(`${cliente1.nome} transferiu R$${valorTransferido} para ${cliente2.nome}`)
+console.log(ContaCorrente1);
+console.log(ContaCorrente2);
